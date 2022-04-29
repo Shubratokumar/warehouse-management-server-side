4ffe9f9271f5b3f4f0d059cc81e1b5e7ca8a9e9e
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-29 20:52:37 +0600

    MongoDB conntected to the server

--- index.js
+++ index.js
@@ -8,10 +8,23 @@ const port = process.env.PORT || 5000;
 app.use(cors())
 app.use(express.json())
 
+// connect MongoDB
 
-// text the server 
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const uri = "mongodb+srv://shubrato:<password>@cluster0.gzn4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+client.connect(err => {
+  const collection = client.db("test").collection("devices");
+  // perform actions on the collection object
+  console.log("connected DB")
+//   client.close();
+});
+
+
+
+// test the server 
 app.get('/', (req,res)=>{
-    res.send("wearhouse server is running.")
+    res.send("Wearhouse server is running.")
 })
 
 // Listening the port
