3b1daa15208e6caf195ebd7d27aebb174503d5fb
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-29 18:14:57 +0600

    middleware added

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 // warehouse management server
 const express = require("express");
 const app = express();
+const cors = require("cors");
 const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors())
+app.use(express.json())
 
 
 // text the server 
