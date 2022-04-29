b7f7c3e435bdc0d12036f783fff6ca51694d3e3b
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-29 22:14:05 +0600

    load data from database by using get method

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 // warehouse management server
 const express = require("express");
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
+require('dotenv').config()
 const cors = require("cors");
 const port = process.env.PORT || 5000;
 
@@ -9,16 +11,30 @@ app.use(cors())
 app.use(express.json())
 
 // connect MongoDB
-
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://shubrato:<password>@cluster0.gzn4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gzn4t.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  console.log("connected DB")
-//   client.close();
-});
+
+async function run() {
+    try {
+      await client.connect();
+      const productCollection = client.db("wearhouseManagement").collection("product");
+
+    //  GET : Read data from database
+    // 
+    app.get("/product", async(req, res)=>{
+        const query = {};
+        const cursor = productCollection.find(query)
+        const result = await cursor.toArray()
+        res.send(result)
+    })
+
+    // 
+      
+    } finally {
+
+    }
+  }
+  run().catch(console.dir);
 
 
 
