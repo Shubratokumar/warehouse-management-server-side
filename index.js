0cb54db5b384187026648e27ff6a669af08549ed
Shubrato Kumar <shubratokumargharami920@gmail.com>
2022-04-29 23:44:18 +0600

    server site CRUD operation done

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // warehouse management server
 const express = require("express");
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 require('dotenv').config()
 const cors = require("cors");
@@ -20,15 +20,47 @@ async function run() {
       const productCollection = client.db("wearhouseManagement").collection("product");
 
     //  GET : Read data from database
-    // 
-    app.get("/product", async(req, res)=>{
+    // http://localhost:5000/products
+    app.get("/products", async(req, res)=>{
         const query = {};
         const cursor = productCollection.find(query)
         const result = await cursor.toArray()
         res.send(result)
     })
 
-    // 
+    // POST : Create new product
+    // http://localhost:5000/product
+    app.post("/product", async(req, res)=>{
+        const data = req.body;
+        const result = await productCollection.insertOne(data);
+        res.send(result)
+    })
+
+    // PUT : Update a specific product
+    // http://localhost:5000/product/${id}
+    app.put("/product/:id", async(req, res)=>{
+        const id = req.params.id;
+        const data = req.body;
+        const filter = {_id: ObjectId(id)};
+        const options = { upsert: true };
+        const  updateProduct ={
+            $set : {
+                ...data
+            }
+        };
+        const result = await productCollection.updateOne(filter, updateProduct, options);
+        res.send(result)
+    })
+
+    // DELETE : Delete a specific product
+    // http://localhost:5000/product/${id}
+    app.delete("/product/:id", async(req,res)=>{
+        const id = req.params.id;
+        const query = {_id: ObjectId(id)};
+        const result = await productCollection.deleteOne(query);
+        res.send(result)
+    })
+
       
     } finally {
 
